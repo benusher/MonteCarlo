b912ec38419f922279f6919b11e1026152a5bc9b
Ben Usher <ben_usher@hotmail.com>
2011-08-19 09:29:46 +0100

    Done! But it's horrible :(

--- src/main/java/com/iskido/montecarlo/MonteCarloRunner.java
+++ src/main/java/com/iskido/montecarlo/MonteCarloRunner.java
@@ -9,8 +9,12 @@ public class MonteCarloRunner {
 
     public static void main(String[] args) {
         TaskDurationHistories taskDurationHistories = new TaskDurationHistories();
-        taskDurationHistories.with(Task.SMALL, new TaskDurationHistory(standardDays(1), standardDays(1), standardDays(1), standardDays(2)));
-        taskDurationHistories.with(Task.MEDIUM, new TaskDurationHistory(standardDays(2), standardDays(3), standardDays(4)));
+
+        TaskDurationHistory smallTasks = smallTasks();
+        taskDurationHistories.with(Task.SMALL, smallTasks);
+
+        TaskDurationHistory mediumTasks = mediumTasks();
+        taskDurationHistories.with(Task.MEDIUM, mediumTasks);
 
         MonteCarloMethod monteCarloMethod = new MonteCarloMethod(taskDurationHistories);
 
@@ -20,11 +24,41 @@ public class MonteCarloRunner {
         tasks.add(Task.SMALL);
         tasks.add(Task.SMALL);
         tasks.add(Task.SMALL);
+        tasks.add(Task.SMALL);
+        tasks.add(Task.SMALL);
 
         Outcomes outcomes = monteCarloMethod.simulateFor(tasks);
 
+        int total = 0;
         for (Outcome outcome : outcomes.asList()) {
-            System.out.println(standardDaysIn(outcome.getDuration().toPeriod()).getDays() + ", " + outcome.getFrequency());
+            total += outcome.getFrequency();
+            System.out.println(standardDaysIn(outcome.getDuration().toPeriod()).getDays() + ", " + total/10);
         }
+    }
+
+    private static TaskDurationHistory smallTasks() {
+        TaskDurationHistory tasks = new TaskDurationHistory();
+        tasks.add(standardDays(1), 14);
+        tasks.add(standardDays(2), 19);
+        tasks.add(standardDays(3), 5);
+        tasks.add(standardDays(4), 8);
+        tasks.add(standardDays(5), 2);
+        tasks.add(standardDays(6), 2);
+        return tasks;
+    }
+
+    private static TaskDurationHistory mediumTasks() {
+        TaskDurationHistory tasks = new TaskDurationHistory();
+        tasks.add(standardDays(1), 0);
+        tasks.add(standardDays(2), 1);
+        tasks.add(standardDays(3), 4);
+        tasks.add(standardDays(4), 2);
+        tasks.add(standardDays(5), 1);
+        tasks.add(standardDays(6), 0);
+        tasks.add(standardDays(7), 1);
+        tasks.add(standardDays(8), 1);
+        tasks.add(standardDays(9), 1);
+        tasks.add(standardDays(10), 2);
+        return tasks;
     }
 }
--- src/main/java/com/iskido/montecarlo/TaskDurationHistory.java
+++ src/main/java/com/iskido/montecarlo/TaskDurationHistory.java
@@ -2,19 +2,28 @@ package com.iskido.montecarlo;
 
 import org.joda.time.Duration;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class TaskDurationHistory {
 
-    private final Duration[] taskDurations;
+    private final List<Duration> taskDurations;
     private final Random random;
 
     public TaskDurationHistory(Duration... taskDurations) {
-        this.taskDurations = taskDurations;
+        this.taskDurations = new ArrayList<Duration>(Arrays.asList(taskDurations));
         random = new Random();
     }
 
     public Duration getTaskDuration() {
-        return taskDurations[random.nextInt(taskDurations.length)];
+        return taskDurations.get(random.nextInt(taskDurations.size()));
+    }
+
+    public void add(Duration duration, int count) {
+        for (int i = 0; i < count; i++) {
+            taskDurations.add(duration);
+        }
     }
 }
