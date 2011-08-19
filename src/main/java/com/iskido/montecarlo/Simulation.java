4e12a54aa3b605e3966ccac278bfd678377dd0ae
Ben Usher <ben_usher@hotmail.com>
2011-08-19 09:14:45 +0100

    Added three pairs to each simulation

--- src/main/java/com/iskido/montecarlo/Simulation.java
+++ src/main/java/com/iskido/montecarlo/Simulation.java
@@ -2,6 +2,7 @@ package com.iskido.montecarlo;
 
 import org.joda.time.Duration;
 
+import java.util.HashSet;
 import java.util.Queue;
 
 import static org.joda.time.Duration.standardDays;
@@ -17,13 +18,48 @@ public class Simulation {
     }
 
     public Duration run() {
-        Duration totalDuration = standardDays(0);
+        HashSet<Pair> pairs = new HashSet<Pair>();
+        pairs.add(new Pair());
+        pairs.add(new Pair());
+        pairs.add(new Pair());
 
         while(tasks.peek() != null) {
             TaskDurationHistory taskDurationHistory = taskDurationHistories.getDurationHistoryFor(tasks.remove());
-            totalDuration = totalDuration.plus(taskDurationHistory.getTaskDuration());
+
+            Pair leastWorked = pairs.iterator().next();
+            for (Pair pair : pairs) {
+                if (pair.getTotalDuration().isShorterThan(leastWorked.getTotalDuration())) {
+                    leastWorked = pair;
+                }
+            }
+
+            leastWorked.plus(taskDurationHistory.getTaskDuration());
         }
 
-        return totalDuration;
+        Pair mostWorked = pairs.iterator().next();
+        for (Pair pair : pairs) {
+            if (pair.getTotalDuration().isLongerThan(mostWorked.getTotalDuration())) {
+                mostWorked = pair;
+            }
+        }
+
+        return mostWorked.getTotalDuration();
+    }
+
+    private static class Pair {
+
+        private Duration totalDuration;
+
+        public Pair() {
+            this.totalDuration = standardDays(0);
+        }
+
+        public Duration getTotalDuration() {
+            return totalDuration;
+        }
+
+        public void plus(Duration duration) {
+            totalDuration = totalDuration.plus(duration);
+        }
     }
 }
